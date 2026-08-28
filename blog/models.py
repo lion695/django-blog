a71ba6246f1601693e00346dbda99eb051bf8553
lion695 <keron891@gmail.com>
2026-08-28 17:02:10 +0100

    Add a Post model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User  # Import the User model
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+class Post(models.Model):
+    """
+    Stores a single blog post entry related to :model:`auth.User`.
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
--- codestar/settings.py
+++ codestar/settings.py
@@ -27,9 +27,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeinstitute-ide.net",
+    "https://*.herokuapp.com"
+]
+
 
 
 # Application definition
