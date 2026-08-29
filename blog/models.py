d00966762b4e014daeb6bc0fecba037753cfc73a
lion695 <keron891@gmail.com>
2026-08-29 16:06:48 +0100

    Add updated_on field to Post model and run migrations

--- blog/models.py
+++ blog/models.py
@@ -9,9 +9,15 @@ class Post(models.Model):
     """
     Stores a single blog post entry related to :model:`auth.User`.
     """
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    title = models.CharField(max_length=200, unique=True) #This is a string field
+    slug = models.SlugField(max_length=200, unique=True)  #This is a string field
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts') #This is a User Object instance (maps to an integer ID)
+    content = models.TextField() #This is a string field
+    created_on = models.DateTimeField(auto_now_add=True) #This is a datetime object field
+    status = models.IntegerField(choices=STATUS, default=0) #This is an integer field
+
+# Add your new field right here:
+    updated_on = models.DateTimeField(auto_now=True)
+
+    # Add your new optional summary field here:
+    excerpt = models.TextField(blank=True, null=True)
