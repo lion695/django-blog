26853bc270073e432720e1618d3e15247255f465
lion695 <keron891@gmail.com>
2026-08-29 17:42:02 +0100

    Create and register Comment model with database migrations applied

--- blog/models.py
+++ blog/models.py
@@ -9,15 +9,22 @@ class Post(models.Model):
     """
     Stores a single blog post entry related to :model:`auth.User`.
     """
-    title = models.CharField(max_length=200, unique=True) #This is a string field
-    slug = models.SlugField(max_length=200, unique=True)  #This is a string field
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts') #This is a User Object instance (maps to an integer ID)
-    content = models.TextField() #This is a string field
-    created_on = models.DateTimeField(auto_now_add=True) #This is a datetime object field
-    status = models.IntegerField(choices=STATUS, default=0) #This is an integer field
-
-# Add your new field right here:
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
-
-    # Add your new optional summary field here:
     excerpt = models.TextField(blank=True, null=True)
+
+class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`blog.Post` 
+    and :model:`auth.User`.
+    """
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
